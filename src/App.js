b35adaa3c42c139303daee9ee57ded00edb28d18
stinkybrew <wille.tuovinen@metropolia.fi>
2019-02-23 19:57:54 +0200

    save result to localstorage works
    save result to localstorage works

--- src/App.js
+++ src/App.js
@@ -5,38 +5,50 @@ class App extends Component {
     temp:"",
     humidity:"",
     image:"",
-    error:""
+    error:"",
+    list: []
   }
   handleClick = async(e) => {
+    // Get weather button handler
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     if(city && country){
+      // if city and country are inserted correctly...
       const api = await fetch(`http://api.wunderground.com/api/73b0005b139fa130/conditions/q/${country}/${city}.json`); 
-      const rtr = await api.json();
+      const data = await api.json();
       this.setState({
-        temp:rtr.current_observation.temp_c,
-        humidity:rtr.current_observation.relative_humidity,
+        temp:data.current_observation.temp_c,
+        humidity:data.current_observation.relative_humidity,
+        image:data.current_observation.icon_url,
         error:""
       });
     }
+
     else {
+      // set states to "", if error is given
       this.setState({
         image:"",
-        location:"",
+        humidity:"",
         temp:"",
         error:"Please enter city and country details correctly"
       });
     }
   }
   saveClick = () => {
+    // save weather-data button to localtorage
     var fs = require('fs');
-    fs.writeFile('savedtemp.txt', this.state.temp, function (err) {
+    localStorage.setItem("temp", this.state.temp)                      
+    localStorage.setItem("humid", this.state.humidity)
+    localStorage.setItem("weatherimg", this.state.image)
+
+    // save data to .txt file
+    fs.writeFile('savedtemp.txt', this.state.temp, function (err) {    
         if (err) 
-            return console.log(err);
+          return console.log(err);
         console.log('file was saved!');
-    });
-  }
+    }); 
+  } 
   
   render() {
     return (
@@ -49,11 +61,14 @@ class App extends Component {
             <input type="text" placeholder="enter country" id="Country" name="country" className="form-control"></input><br></br>
             <button className="btn btn-info">Get weatherforecast</button>
           </form>
-          {this.state.error!==""?<div class="alert alert-primary" role="alert">{this.state.error}</div>:""}
-          {this.state.temp!==""?<h2>temp:{this.state.temp}</h2>:""}
-          {this.state.humidity!==""?<h2>humidity:{this.state.humidity}</h2>:""}
+          {this.state.temp!==''?
+          <div id="bck"><div className="row">
+            <div className="col-md-4"></div>
+            <div className="col-md-4"><center><img src={this.state.image} width="100px" height="100px"/><br></br><h3 id="h1">Temperature: {this.state.temp}<br></br>Humidity: {this.state.humidity}</h3></center></div>
+          </div></div>:''}
+
           <form onClick={this.saveClick}>
-            <button  id="buttonz" className="btn btn-light">save result</button>
+            <button  id="buttonz" className="btn btn-dark">save result</button>
           </form>
         </div> 
       </center>
